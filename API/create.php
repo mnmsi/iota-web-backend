06a85b27b8d2e1ff49a813e7f1d86e43eaea4c45
HmHasan <49553112+HmHasan@users.noreply.github.com>
2022-03-08 15:23:08 +0600

    allow headers

--- API/create.php
+++ API/create.php
@@ -1,11 +1,8 @@
 <?php
 // headers
-header("Access-Control-Allow-Origin: *");
-header('Content-Type: application/json');
-header("Access-Control-Allow-Methods:POST");
-header("Access-Control-Headers:Access-Control-Headers,Access-Control-Allow-Methods,Content-Type,Access-Control-Allow-Origin,Access-Control-Allow-Methods,Authorization,X-Requested-With");
-
-
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Access-Control-Allow-Headers, X-Requested-With");
 include_once "../config/Database.php";
 include_once "../Models/Post.php";
 
