1a39773fc41353b43e94a66b0d27d9f31f6d8689
HmHasan <49553112+HmHasan@users.noreply.github.com>
2022-03-08 13:59:29 +0600

    try cross

--- API/create.php
+++ API/create.php
@@ -1,6 +1,6 @@
 <?php
 // headers
-header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json');
 header("Access-Control-Allow-Methods:POST");
 header("Access-Control-Headers:Access-Control-Headers,Access-Control-Allow-Methods,Content-Type,Access-Control-Allow-Origin,Access-Control-Allow-Methods,Authorization,X-Requested-With");
